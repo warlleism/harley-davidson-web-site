3abd8387b78c687c049fc3a37f2db9356ed022f2
warllei <warllei.s.mrtins@hotmail.com>
2022-11-24 10:47:15 -0300

    animação na tela de vehicles

---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.scss'
+
+const Footer = () => {
+
+    return (
+        <div className="footer-main">
+         
+        </div>
+    )
+}
+
+export default Footer;
--- src/components/vehicle/index.js
+++ src/components/vehicle/index.js
@@ -2,43 +2,55 @@ import React from "react";
 import "./style.scss"
 
 const Vehicle = () => {
+
+    window.addEventListener('scroll', () => {
+        const tela = parseInt(window.scrollY)
+
+        const img = document.querySelectorAll("#img")
+
+        if (tela >= 1389) {
+            img[0].style.transform = 'translateX(0px)'
+
+            setTimeout(() => {
+                img[1].style.transform = 'translateX(0px)'
+            }, 1200)
+            setTimeout(() => {
+                img[2].style.transform = 'translateX(0px)'
+            }, 2500)
+        }
+
+
+
+    })
+
     return (
         <div className="vehicle-main">
             <div className="titulo">Encontre seu próximo veículo</div>
 
             <div className="vehicle-component">
 
-                <div className="container-info">
+                <div className="container-info" id="img" style={{ zIndex: "1" }}>
                     <img src={require("../../image/vehicle/vehicle-1.png")} />
                     <div className="titulo-vehicle">Road King</div>
                     <div className="desc-vehicle">
-                        A Road King™ Special 2022, uma bagger
-                        customizada com o icônico estilo H-D, é
-                        uma hot rod customizada na fábrica com
-                        potência topo de linha e uma presença
-                        impositiva.
+                        Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.
                     </div>
                 </div>
 
 
-                <div className="container-info">
+                <div className="container-info" id="img" style={{ zIndex: "2" }}>
                     <img src={require("../../image/vehicle/vehicle-2.png")} />
                     <div className="titulo-vehicle">Fat Bob</div>
                     <div className="desc-vehicle">
-                        Construída com estilo e desempenho reforçados,
-                        a Fat Bob™ 114 2022 é uma devoradora de ruas
-                        que irá satisfazer os mais exigentes motociclistas com fome de desempenho.
+                        Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.
                     </div>
                 </div>
 
-                <div className="container-info">
+                <div className="container-info" id="img" style={{ zIndex: "3" }}>
                     <img src={require("../../image/vehicle/vehicle-3.png")} />
                     <div className="titulo-vehicle">ROAD GLIDE"&nbsp;  SPECIAL</div>
-                    <div className="desc-vehicle">Como uma bagger de desempenho nas
-                        opções totalmente preta e laranja brilhante,
-                        a Road Glide™ Special 2022 apresenta um design
-                        clássico de touring equipada com recursos
-                        premium.
+                    <div className="desc-vehicle">
+                        Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.
                     </div>
                 </div>
 
